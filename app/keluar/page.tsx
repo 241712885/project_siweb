0051bb1ca29676d3d36c5821555a57d74d85ca55
241712885 <mirandaimanuekka@gnail.com>
2026-04-21 14:54:34 +0700

    SELESAI FIX BGT

--- app/keluar/page.tsx
+++ app/keluar/page.tsx
@@ -6,32 +6,29 @@ export default function KeluarPage() {
 
     return (
         <div className="min-h-screen flex items-center justify-center bg-green-600">
-            <div className="bg-gray-200 w-[500px] h-[600px] rounded-2xl flex items-center justify-center shadow-xl">
-                <div className="border border-green-600 rounded-xl px-10 py-8 text-center bg-white shadow">
-                    <h2 className="text-2xl font-bold text-green-700 mb-2">
-                        Konfirmasi
-                    </h2>
-                    <p className="text-gray-600 mb-6">
-                        Apakah Anda yakin ingin keluar?
-                    </p>
+            <div className="border border-green-600 rounded-xl px-10 py-8 text-center bg-white shadow">
+                <h2 className="text-2xl font-bold text-green-700 mb-2">
+                    Konfirmasi
+                </h2>
+                <p className="text-gray-600 mb-6">
+                    Apakah Anda yakin ingin keluar?
+                </p>
 
-                    <div className="flex justify-center gap-6">
-                        <button
-                            onClick={() => router.back()}
-                            className="px-6 py-2 border border-green-600 text-green-600 rounded-lg hover:bg-green-50"
-                        >
-                            Tidak
-                        </button>
+                <div className="flex justify-center gap-6">
+                    <button
+                        onClick={() => router.back()}
+                        className="px-6 py-2 border border-green-600 text-green-600 rounded-lg hover:bg-green-50"
+                    >
+                        Tidak
+                    </button>
 
-                        <button
-                            onClick={() => router.push("/")}
-                            className="px-6 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700"
-                        >
-                            Ya
-                        </button>
-                    </div>
+                    <button
+                        onClick={() => router.push("/")}
+                        className="px-6 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700"
+                    >
+                        Ya
+                    </button>
                 </div>
-
             </div>
         </div>
     );
