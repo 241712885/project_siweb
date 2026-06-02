69d8d68ab88e3a434c3234dc10115abdc7d008f4
241712885 <mirandaimanuekka@gnail.com>
2026-06-02 08:54:39 +0700

    FINALLLLLLYy

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -4,4 +4,4 @@ neonConfig.fetchEndpoint = (host) => {
   return `https://${host}/sql`;
 };
 
-export const sql = neon(process.env.DATABASE_URL!);
+export const sql = neon(process.env.NEON_NEW_DATABASE_URL || process.env.DATABASE_URL!);
